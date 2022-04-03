ee92a48a9c304078ae4bcdc87380aacae6793c11
omokhele <anne.imomoh@gmail.com>
2022-04-03 13:19:46 -0700

    Stocks App Update

--- src/components/Stocks.js
+++ src/components/Stocks.js
@@ -6,32 +6,38 @@ import { CLIENT_URL}  from '../services/constants';
 import Header from './Header'
 
 function Stocks () {
-    let syms = ['FB', 'aapl' , 'snap', 'NDAQ'];
-    // https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_92a7c44683df4731b5a113fd3561a40d
-    const [stockDatas, setStockDatas] = useState([]);
-    const [symbol, setSymbol] = useState(syms)
     
+
+    // // https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_92a7c44683df4731b5a113fd3561a40d
+
+    const [stockDatas, setStockDatas] = useState([]);
+    // const [symbol, setSymbol] = useState('')
+    // const symbol = 'aapl'
     const key = '/quote?token=pk_92a7c44683df4731b5a113fd3561a40d'
+    
+    const syms = ['FB', 'aapl' , 'snap', 'NDAQ'];
     useEffect(() => {
-        const fetchData = async () => {       
+        const fetchData = async (item) => { 
+            
+                         
+                   
             try{
-                const response = await axios.get(`${CLIENT_URL}${symbol}${key}`);
+                const response = await axios.get(`${CLIENT_URL}${item}${key}`);
                 console.log(response.data) 
                 const result = []
                 result.push(response.data);
-                setStockDatas(result);
-                setSymbol(symbol);
+                setStockDatas((prevState) => prevState.concat(result));
+                // setSymbol(symbol);
             }catch(error) {
                 console.log(error)
             }
         }
-        fetchData();
+        const item = syms.map(fetchData); 
+                console.log(item) 
         console.log("running...")
     },[])
 
-    syms.map((sym) => {
-        return sym
-    })
+    
 
     return (
        <div>
@@ -44,7 +50,7 @@ function Stocks () {
                (stockDatas || []).map((stockData, index) => {
                    const { symbol, companyName, latestPrice, ytdChange } = stockData;
                 return (<Header 
-                    symbol = {symbol} 
+                    symbol= {symbol} 
                     companyName = {companyName} 
                     latestPrice = {latestPrice} 
                     ytdChange = {ytdChange} 
